228cf9a0d5e16b93af838f1320091967c6884e13
Josh Trujillo <josh@Joshs-iMac.local>
2017-11-28 16:03:56 -0800

    added devil's advocate

--- corrections/check_corrections.rb
+++ corrections/check_corrections.rb
@@ -10,7 +10,7 @@ session = GoogleDrive::Session.from_config("client_secret.json")
 
 RC_C = session.spreadsheet_by_key("1dxPaBz2RvAIhUKnQmXDyRsA26z5bvOCGrJvqJrapKb4").worksheets[0]
 RC_A = session.spreadsheet_by_key("1wQA-jDQfux5K8Y7mkcYtn7MKgkf3GUL1bHAJ7F_Frbw").worksheets[0]
-
+RC_AP = session.spreadsheet_by_key("1wQA-jDQfux5K8Y7mkcYtn7MKgkf3GUL1bHAJ7F_Frbw").worksheets[1]
 # authenticates slack token
 Slack.configure do |config|
 	config.token = ENV['SLACK_API_TOKEN']
@@ -71,6 +71,7 @@ def if_match(teams, start)
 	return match
 end
 
+# prints list of every correction from a given week 
 def print_from(start)
 	while start < RC_C.num_rows
 		puts RC_C.rows[start][1]
@@ -78,8 +79,29 @@ def print_from(start)
 	end
 end
 
-# puts get_teams - if_match(get_teams, week_start)
-# puts JSON.pretty_generate(client.users_info(user: '@jtrujill'))
-id = client.users_info(user: '@jtrujill')[:user][:id]
+#appends x to missed correction cell
+def no_correction(teams)
+	teams.each do |team|
+		i = 0
+		j = RC_AP.num_cols
+		while i < RC_AP.num_rows
+			if RC_AP.rows[i][5] == team
+				tmp = RC_AP[(i + 1), j] + 'x'
+				RC_AP[(i + 1), j] = tmp
+			end
+			i += 1
+		end
+	end
+	RC_AP.save 
+end
+		# members = # range of each person on RC_A worksheet 2
+		# if person team == team
+		# 		message on slack
+
+missed_teams = get_teams - if_match(get_teams, week_start)
+no_correction(missed_teams)
+puts missed_teams
+# puts RC_AP.rows[1][5]
+# id = client.users_info(user: '@jtrujill')[:user][:id]
 #id2 = client.users_info(user: '@rlutt')[:user][:id]
-client.chat_postMessage(channel: id, text: "testing", as_user: true)
+# client.chat_postMessage(channel: id, text: "testing", as_user: true)
---
+++ monday_meetings/devils_advocate.rb
@@ -0,0 +1,68 @@
+require 'google_drive'
+require 'googleauth'
+require 'fileutils'
+require 'date'
+require 'slack-ruby-client'
+
+# authenticate a session with your service account
+session = GoogleDrive::Session.from_config("client_secret.json")
+
+RC_AP = session.spreadsheet_by_key("1wQA-jDQfux5K8Y7mkcYtn7MKgkf3GUL1bHAJ7F_Frbw").worksheets[1]
+
+#authenticates slackbot
+Slack.configure do |config|
+	config.token = ENV['SLACK_API_TOKEN']
+end
+Client = Slack::Web::Client.new
+Client.auth_test
+
+# fetches google sheet generated from form data
+def get_sheet
+	sheet = Array.new
+	i = 1
+	j = 0
+	while i < RC_AP.num_rows
+		if RC_AP.rows[i][5] != 'Admin' && RC_AP.rows[i][5] != 'other' && RC_AP.rows[i][RC_AP.num_cols - 2] != 'Excused'
+			sheet[j] = Array.new
+			sheet[j][0] = RC_AP.rows[i][2]
+			sheet[j][1] = RC_AP.rows[i][5]
+			j += 1
+		end
+		i += 1
+	end
+	return sheet
+end
+
+
+# takes in list of usernames to contact and messages them on slack
+def contact_devil(teams)
+	teams.each do |team|
+		id = Client.users_info(user: '@' + team)[:user][:id]
+		puts 'login is ' + team + ', id is ' + id
+		Client.chat_postMessage(channel: 'rc_dev', text: 'You have been selected to play the Devil\'s Advocate role at Rabbit Cloud\'s weekly meeting. You will be one of 3 people tasked with this role and your job is to ensure each team is challenged with atleast 1 question during their presentation. Please let me know if you cannot make this meeting or perform this role. Thanks', as_user: true)
+	end
+end
+
+# randomly finds 3 students from unique teams
+def find_devil
+	people = get_sheet.shuffle
+	teams = Array.new
+	res = Array.new
+	i = 0
+	j = 0
+	while i < 3
+		if teams.include?(people[j][1]) == false
+			res.push(people[j][0])
+			teams.push(people[j][1])
+			i += 1
+		end
+		j+= 1
+	end
+	return res
+end
+
+if ARGV[0] == 'production'
+	contact_devil(find_devil)
+else
+	puts "Usage: ruby devils_advocate.rb \"production\""
+end
